883da849f89057acab9a5a938d696501c424ae9f
praveen10100000 <praveenpandey.uttarakhand@gmail.com>
2022-05-20 15:58:53 +0530

    change

--- src/main/java/com/shield/Shield/entity/Staff.java
+++ src/main/java/com/shield/Shield/entity/Staff.java
@@ -1,7 +1,15 @@
 package com.shield.Shield.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import net.bytebuddy.asm.Advice;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Staff {
+
 }
